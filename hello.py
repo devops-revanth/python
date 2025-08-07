3e628242e766ee2e4f69bda1c4410349fd0d953b
devops-revanth <revanth1226@gmail.com>
2025-08-07 20:37:45 +0530

    test

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+# Variable
+
+#Interger
+result=10+20
+#print(10+20)
+print("Result: ",result)
+print("Memory Location: ", id)
+print("Data type: ", type(result))
+
+x="Hello World"
+print(x.split(' '))
+print(x.find('l'))
