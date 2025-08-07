1aa344673bd8010fafd87298743ecfbe653a75df
devops-revanth <revanth1226@gmail.com>
2025-08-07 20:45:50 +0530

    python

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("Hello Linux admin!")
+
+print("We are creating System Monitoring Script")
+
+log_path="/var/log/messages"
+print(type(log_path))
+hostname="mytestvm"
+print(type("mytestvm"))
+
