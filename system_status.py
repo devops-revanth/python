62a805535fec7df8337586c38d9fad44d05102d6
devops-revanth <revanth1226@gmail.com>
2025-08-07 21:53:32 +0530

    python

---
+++ system_status.py
@@ -0,0 +1,16 @@
+import os
+
+hostname= os.uname()
+
+print(hostname)
+
+
+# print("Hostname:" , hostname)
+
+# cpu_load = os.getloadavg()[0]
+# print("Current Load Average:" , cpu_load)
+
+# if cpu_load > 2.0:
+#     print("High Load")
+# else:
+#     print("Load Normal")
