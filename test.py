bef858559450faddd55af9c61afd5572c9988386
devops-revanth <revanth1226@gmail.com>
2025-08-07 20:48:27 +0530

    python

--- test.py
+++ test.py
@@ -3,7 +3,12 @@ print("Hello Linux admin!")
 print("We are creating System Monitoring Script")
 
 log_path="/var/log/messages"
-print(type(log_path))
+# print(type(log_path))
 hostname="mytestvm"
-print(type("mytestvm"))
+# print(type("mytestvm"))
+
+print("Disk Usage is High")
+
+name=input("Enter your server name")
+print("Running health check on", name)
 
