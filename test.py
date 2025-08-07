33be8b840607a33965cdc21d7509f8cc0ef6793b
devops-revanth <revanth1226@gmail.com>
2025-08-07 21:03:15 +0530

    python

--- test.py
+++ test.py
@@ -36,3 +36,8 @@ while count < 3:
     count += 1 # same as "count = count + 1"
 
 
+def check_disk(disk):
+    print("Checking usage of disk" , disk)
+    print(type(check_disk))
+
+check_disk("/")
