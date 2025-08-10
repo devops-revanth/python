48ce6793093fa431c4d7eb5c4cebe06f3f4e9a03
devops-revanth <revanth1226@gmail.com>
2025-08-11 01:09:31 +0530

    python

--- 09-All-python.py
+++ 09-All-python.py
@@ -29,3 +29,8 @@ Strip_text = "         Python did well for strip          "
 strip = Strip_text.strip()
 print(strip)
 
+Text = "Python is awesome"
+substring = is
+if substring is in Text:
+    print(substring , in your text)
+
