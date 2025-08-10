19e86653d83949fba47530fea147f0a06f4f6132
devops-revanth <revanth1226@gmail.com>
2025-08-11 01:00:56 +0530

    python

---
+++ 09-All-python.py
@@ -0,0 +1,24 @@
+text = "Python is awesome"
+
+A = "Hello"
+B = "World"
+
+#Concatination
+#Split
+#Substring
+#Upper
+#Lower
+#Strip
+#Len
+#Replace
+
+length = text.len()
+split = text.split(" ")
+lower = text.lower()
+upper = text.upper()
+replace = text.replace("awesome" , "fun")
+Concatination = A + " " + B
+
+Strip_text = "         Python did well for strip          "
+strip = Strip_text.strip()
+
