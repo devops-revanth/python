1c8ae6a0ae0094507e2a16b7e2b8e9dca3a53ef0
devops-revanth <revanth1226@gmail.com>
2025-08-11 01:02:22 +0530

    python

--- 09-All-python.py
+++ 09-All-python.py
@@ -13,6 +13,7 @@ B = "World"
 #Replace
 
 length = text.len(text)
+print("Length of text:" , length)
 split = text.split(" ")
 lower = text.lower()
 upper = text.upper()
@@ -21,4 +22,5 @@ Concatination = A + " " + B
 
 Strip_text = "         Python did well for strip          "
 strip = Strip_text.strip()
+
 
