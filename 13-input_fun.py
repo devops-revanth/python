c4cb2aacf42b359761b33366019c457b0ee3e91b
devops-revanth <revanth1226@gmail.com>
2025-08-12 05:28:09 +0530

    shell

---
+++ 13-input_fun.py
@@ -0,0 +1,11 @@
+def addition(num1 , num2):
+    add = num1 + num2
+    return add
+def subtraction(num1 , num2):
+    sub = num1 - num2
+    return sub
+def multiplication(num1 , num2):
+    mil = num1 * num2
+    return mul
+
+print(add(num1=10 , num2=5))
