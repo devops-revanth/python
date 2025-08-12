9649dfb186c983733447118a28a0e407f12e4c59
devops-revanth <revanth1226@gmail.com>
2025-08-13 00:48:45 +0530

    shell

---
+++ 14-upto_Day5.py
@@ -0,0 +1,46 @@
+### Day1 Scripts
+
+print("Hello World!")
+
+
+### Day2 Scripts
+
+## Concat
+## Len
+## Upper
+## Lower
+## Split
+## Substring
+## replace
+## Strip
+
+text = "Python is awesome"
+
+str1 = "Hello"
+str2 = "Raj , Name tho sunavo"
+
+Concat = str1 + str2
+print(Concat)
+
+
+length = len(text)
+
+print(length)
+
+Upper = text.upper()
+Lower = text.lower()
+
+Split = text.split()
+print(Split)
+
+replace = text.replace("awesome" , "fun")
+print(replace)
+
+raju = "         Raju is a good boy          "
+strip = raju.strip()
+
+print(strip)
+
+substring = is
+if substring in text:
+    print(substring , "Found in text")
