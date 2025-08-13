2b44d9e724a3000b7bfdebe3b835a7312e071e15
devops-revanth <revanth1226@gmail.com>
2025-08-13 05:38:05 +0530

    shell

--- 15-functions.py
+++ 15-functions.py
@@ -1,27 +1,42 @@
-num1 = 10
-num2 = 5
+# num1 = 10
+# num2 = 5
+# 
+# addition=num1 + num2
+# print(addition)
+# 
+# sub=num1-num2
+# print(sub)
+# 
+# mul=num1 * num2
+# print(mul)
+# 
+# 
+# def addition():
+    # add = num1 + num2
+    # print(add)
+# 
+# def subtraction():
+    # sub = num1 - num2
+    # print(sub)
+# def multiplication():
+    # mul = num1 * num2
+    # print(mul)
+# 
+# addition()
+# subtraction()
+# multiplication()
 
-addition=num1 + num2
-print(addition)
 
-sub=num1-num2
-print(sub)
-
-mul=num1 * num2
-print(mul)
-
-
-def addition():
+def addition(num1 , num2):
     add = num1 + num2
-    print(add)
-
-def subtraction():
+    return add
+def subtraction(num1 , num2):
     sub = num1 - num2
-    print(sub)
-def multiplication():
+    return sub
+def multiplication(num1 , num2):
     mul = num1 * num2
-    print(mul)
+    return mul
 
-addition()
-subtraction()
-multiplication()
+print(addition(10 , 5))
+print(subtraction(10,5))
+print(multiplication(10,5))
