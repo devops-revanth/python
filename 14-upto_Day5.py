2010ec15b8e4767f35850c970b0336375224cc9c
devops-revanth <revanth1226@gmail.com>
2025-10-20 03:26:22 +0530

    new update

--- 14-upto_Day5.py
+++ 14-upto_Day5.py
@@ -1,106 +1,11 @@
-### Day1 Scripts
-
-print("Hello World!")
+print("Hello World")
 
 
-### Day2 Scripts
+str1 = "High file"
+str2 = "Count detected"
+str = "Python is awesome"
 
-### String in-built functions
-## Concat
-## Len
-## Upper
-## Lower
-## Split
-## Substring
-## replace
-## Strip
-
-text = "Python is awesome"
-
-str1 = "Hello"
-str2 = "Raj , Name tho sunavo"
-
-Concat = str1 + str2
-print("Concatincation output" , Concat)
-
-
-length = len(text)
-
-print("Length output " ,length)
-
-Upper = text.upper()
-Lower = text.lower()
-
-print("Upper output" , Upper)
-print("Lower output" , Lower)
-
-Split = text.split()
-print("Split output" , Split)
-
-replace = text.replace("awesome" , "fun")
-print("Replace out put : " , replace)
-
-raju = "         Raju is a good boy          "
-strip = raju.strip()
-
-print("Stripping output  " , strip)
-
-substring = "is"
-if substring in text:
-    print("Substring output : " , substring , "Found in text")
-
-
-#################################################
-
-### Float and integer
-
-a = 10
-b = 3
-c=-5
-
-add = a+b
-sub = a-b
-mul = a*b
-div = a/b
-
-
-
-print(add)
-print(sub)
-print(mul)
-print(div)
-
-r= round(div , 2)
-print(r)
-
-
-mod = a%b
-divi=a//b
-abs=abs(c)
-
-print(mod)
-print(divi)
-print(abs)
-
-
-######################################################
-
-##Day3
-my_variable=42
-print(my_variable)
-
-## Local variable
-def my_function():
-    x=10
-    print(x)
-my_function()
-
-## Global variable
-y=20
-def another_function():
-    print(y)
-another_function()
-print(y)
-
-####################################################
+#Concatination
+result1 = str1 + str2 # Concatination
+print("This result is for Concatination : " , result1)
 
