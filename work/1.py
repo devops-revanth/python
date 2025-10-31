d1ef78fd6f374e3627c6fa2bdc1997e4994b277d
devops-revanth <revanth1226@gmail.com>
2025-10-31 23:43:31 +0530

    latest

---
+++ work/1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+hostname = "auto2"
+cpu_threshold = 85
+
+"""
+This is for learning python3 for Revanth Kumar G
+will practice all , one by one
+"""
+
+username=input("Enter username:")
+
+print(f"User: {username}")
