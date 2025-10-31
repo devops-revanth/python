b5600cdd3b574d21032add4db169a9d69446b6df
devops-revanth <revanth1226@gmail.com>
2025-11-01 02:38:40 +0530

    latest

--- work/3.linux.py
+++ work/3.linux.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 import os
+import time
 
 for log in os.listdir("/var/log"):
-    if log.endswith(".log"):
+    if log.endswith(".log"):   #startswith(prefix) , endswith(suffix) , replace(old,new) , split , strip , find , lower , upper , os.path.isfile , os.path.isdir , os.path.getsize , os.path.exists
         print(f"Found : {log}")
+
+
+
+for i in range(3):
+    print(f"Attempt {i+1} to connect")
+    time.sleep(2)
