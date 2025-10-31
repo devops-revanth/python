18327920a37760c06f990109d6e0046bf3abc26c
devops-revanth <revanth1226@gmail.com>
2025-10-31 23:54:26 +0530

    latest

--- work/1.py
+++ work/1.py
@@ -9,10 +9,22 @@ This is for learning python3 for Revanth Kumar G
 will practice all , one by one
 """
 
-username = input("Enter you name : ")
-print(f"You name is : {username}")
+#username = input("Enter you name : ")
+#print(f"You name is : {username}")
 
 
 hostname = os.uname()[1]
 
 print(f"Your running hostname is : {hostname}")
+
+
+"""
+Keywords : if , elif , for , while , break , continue 
+           def, return , import , from , as ,
+           try , except , finally , raise
+           with , true , False , None
+"""
+
+print(os.getcwd())
+print(os.getenv("HOME"))
+
