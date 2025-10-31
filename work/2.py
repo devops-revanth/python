5a0a3df8a5312015892150ff5f5913215b8feed5
devops-revanth <revanth1226@gmail.com>
2025-11-01 02:03:28 +0530

    latest

--- work/2.py
+++ work/2.py
@@ -40,3 +40,56 @@ mounts = ["/" , "/tmp" , "/data" , "/home"]
 
 for m in mounts:
     print(f"checking the disk usage : {m}")
+
+
+#tuple
+iface = ("eth0","UP")
+print(f"{iface[0]} status {iface[1]}")
+
+
+#Dict
+server_stats = {"cpu":65, "mem":72,"disk":81}
+print(f"Disk Usage: {server_stats['disk']}%")
+print(f"Cpu usage : {server_stats['cpu']}%")
+print(f"Memory usage : {server_stats['mem']}%")
+
+#set
+servers = {"node1" , "node2" , "node3"}
+print(f"{servers}")
+
+
+"""
+Arithmetic - +,-,/,%,//,** - Disk Usage Calculations
+Comparison -> == != > < >= <= - threshold checks
+Logical and or not   - Multi Condition tests
+Assignment = += -= *=  - update values
+Membership  - in notin   - check item in list/dict
+identity -- is , isnot   - compare object refs
+Bitwise - & - ^ ~ << >>
+"""
+
+
+disk_usage = 82
+if disk_usage > 80:
+    print("ALERT: Disk usage is Critical")
+
+
+cpu=75
+mem=70
+if cpu>70 and mem>60:
+    print(f"High Load Detected")
+
+
+
+#List 
+servers=["web1" , "web2"]
+servers.append("db1")
+print(f"{servers}")
+
+
+"""
+strings
+lists
+numbers
+dictionaries
+"""
---
+++ work/3.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+"""
+if condition:
+    #code_block
+elif another_condition:
+    #code_block
+else:
+    #default block
+
+"""
+
+#Disk Usage Alert
+
+disk_usage = 85
+
+if disk_usage>90:
+    print(f"Critical : Disk Usage above 90#")
+elif disk_usage>85:
+    print(f"Warning : Disk usage above 80%")
+else:
+    print(f"OK : Disk usage is normal")
+
+#SSHD server check
+service="sshd"
+status="running"
+
+if status == "running":
+    print(f"{service} is active")
+else:
+    print(f"{service} is down!")
+
+#Nested conditions
+cpu = 70
+mem = 85
+
+if cpu > 80:
+    if mem>70:
+        print(f"High CPU amd Memory utilization")
+
+
+
+#Loops
+mounts=["/","/home","/data"]
+
+for m in mounts:
+    print(f"Disk utilization of {m}")
+
+
+usage={"cpu":70, "mem":60 , "disk":85}
+for key, value in usage.items():
+    print(f"{key.upper()} usage = {value}%")
+
+import time
+service_up = False
+attempt = 0
+while not service_up and attempt < 5:
+    print(f"Checking service status ...")
+    attempt += 1
+    time.sleep(2)
+
+print(f"Service check completed")
